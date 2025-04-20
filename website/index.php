0b80c3c9a193101412fd0c185d2d4ce85060d53a
lucky-israel <71261428+lucky-israel@users.noreply.github.com>
2025-04-20 21:45:04 +0100

    Update index.php

---
+++ website/index.php
@@ -0,0 +1,8 @@
+## Demo Website
+
+This folder contains the static PHP/HTML code deployed to `/var/www/html`.
+
+File: `index.php`
+```php
+<h1>Welcome to israel bamidele’s Secure Server 🚀</h1>
+<p>The current server time is: <?php echo date('Y-m-d H:i:s'); ?></p>
